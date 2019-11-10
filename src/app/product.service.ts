43ced9a4683b2dd1aa911d47d3385ff5eec4e828
Mudiaga Obriki <mudi_invents@yahoo.com>
2019-11-10 15:50:22 +0100

    Task 3- 3rd Take Creating a Service to Get Album Data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -7,12 +7,10 @@ export class ProductService {
  private  _albumUrl = '../assets/album.json';
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
+  getAlbum (id: number) {
     return this._http.get(this._albumUrl)
       .map(response => {
         return response.json();
       })
   }
-
-
 }
